5a1b39b0ceb5abc87f04302a07c30e7cf343bc29
martinyis <martinyis11@gmail.com>
2023-07-04 10:40:09 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div class="container mx-auto ">
+      <h1 class="text-4xl font-bold sm:text-2xl">Desktop Heading</h1>
+      <p class="text-lg sm:text-base">Desktop paragraph text</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "420px" },
+      xs: { max: "320px" },
+    },
+  },
+  plugins: [],
+};
